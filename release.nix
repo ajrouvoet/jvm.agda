4328dfdee01d3c340b8d68548845450914d70331
Arjen Rouvoet <a.j.rouvoet@gmail.com>
2020-07-09 10:11:59 +0200

    [ del ] nix build

--- release.nix
+++
@@ -1,9 +0,0 @@
-let
-  pkgs = import ./lib/nixos.nix { json = ./lib/nixos-19.09.json; };
-  haskell = pkgs.haskell.packages.ghc881.override { overrides = self: super: {
-    equivalence = super.callPackage ./lib/equivalence.nix {};
-    regex-tdfa  = super.callPackage ./lib/regex-tdfa.nix {};
-    regex-base  = super.callPackage ./lib/regex-base.nix {};
-  }; };
-in
-  { agda = haskell.callPackage ./lib/agda.nix {}; }
